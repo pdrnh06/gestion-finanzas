915cdba99b7652dddc2d7949747412fa9d1526b5
morphine <pedsamdut486@gmail.com>
2026-02-10 22:31:31 +0100

    Added a function to sort transaction by date descending and improved the system for generating the id.

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 function validarTransaccion(importe,origen,fecha) {
-
     let errores = [];
     let valido;
 
     if (importe == "") {
-        errores.push("Error en el importe.")
-    } if (origen == ""){
-        errores.push("Error en el origen.")
+        errores.push("Error en el importe.");
+    } if (origen == "") {
+        errores.push("Error en el origen.");
     } if (fecha == "") {
-        errores.push("Error en la fecha.")
-    }
+        errores.push("Error en la fecha.");
+    };
 
-    if (errores.length > 0){
+    if (errores.length > 0) {
         valido = false;
     } else {
-        valido = true
-    }
+        valido = true;
+    };
 
     const check = {
         valido: valido,
@@ -26,7 +25,6 @@ function validarTransaccion(importe,origen,fecha) {
 }
 
 function crearTransaccion(importe,origen,fecha) {
-
     const transaccion = {
         id: siguienteId,
         importe: importe,
@@ -34,54 +32,74 @@ function crearTransaccion(importe,origen,fecha) {
         fecha: fecha,
     }
 
-    transacciones.push(transaccion);
+    transaccionesGlobal.push(transaccion);
 
     siguienteId++;
 
-    localStorage.setItem("Transacciones", JSON.stringify(transacciones));
-
+    localStorage.setItem("Transacciones", JSON.stringify(transaccionesGlobal));
     return transaccion;
-}
+};
 
 function leerTransacciones() {
     let transacciones = localStorage.getItem("Transacciones");
     if (transacciones) {
-        transacciones = console.log(JSON.parse(transacciones));
+        transacciones = JSON.parse(transacciones);
         return transacciones;
     } else {
-        console.log("No hay ninguna transacción actualmente.")
         return;
-    }
-    
-}
+    };
+};
 
 function manejarEnvioFormulario() {
-
     let importe = document.forms['formularioTransaccion']['importe'].value;
     let origen = document.forms['formularioTransaccion']['origen'].value;
-    let fecha = document.forms['formularioTransaccion']['fecha'].value;
+    let fecha = document.forms['formularioTransaccion']['fecha'].value + "Z";
 
     let resultado = validarTransaccion(importe,origen,fecha);
 
     let importeNumero = Number(importe);
 
     if (importeNumero == 0){
-        alert("El importe no puede ser 0.")
+        alert("El importe no puede ser 0.");
         return;
     } if (importeNumero < 0){
-        alert("El importe no puede ser negativo.")
+        alert("El importe no puede ser negativo.");
         return;
     }
 
     if (!resultado.valido){
         resultado.errores.forEach(error => {
-            alert(error)
+            alert(error);
         });
         return;
     } else {
-        crearTransaccion(importe,origen,fecha);
-    }
-}
+        crearTransaccion(importe,origen,new Date(fecha));
+        ordenarTransacciones(transaccionesGlobal);
+    };
+};
 
-var transacciones = [];
-var siguienteId = 1;
+function ordenarTransacciones(arrayTransacciones) {
+    arrayTransacciones.sort(function (a,b) {
+        return b.fecha - a.fecha;
+    });
+};
+
+//=======================================================================
+
+var siguienteId;
+var maxId = 0;
+var transaccionesGlobal;
+var datosGuardados = JSON.parse(localStorage.getItem("Transacciones"));
+
+if (datosGuardados){
+    transaccionesGlobal = datosGuardados;
+} else {
+    var transaccionesGlobal = [];
+};
+
+transaccionesGlobal.forEach(transaccion => {
+    if (transaccion.id > maxId) {
+        maxId = transaccion.id;
+        var siguienteId = maxId + 1;
+    };
+});
